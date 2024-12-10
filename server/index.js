5fed4b6caaa71ef0665467588fc167021a56cb9e
Arun Kushwaha <arunsk1310@gmai.com>
2024-12-09 20:08:22 -0500

    check1

--- server/index.js
+++ server/index.js
@@ -16,7 +16,10 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 // CORS Configuration
-const allowedOrigins = ["http://localhost:3000", "https://slot-selection.vercel.app"];
+const allowedOrigins = [
+  "http://localhost:3000", // Local frontend URL for development
+  "https://slot-selection.vercel.app" // Deployed frontend URL
+];
 app.use(cors({
   origin: (origin, callback) => {
     // Allow requests with no origin (e.g., mobile apps or Postman)
